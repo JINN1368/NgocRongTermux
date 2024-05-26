1cab3ea4f97b59b3461c65be5d32fcf89df2e51b
Jinn <52101245+JINN1368@users.noreply.github.com>
2024-05-26 08:58:45 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,74 +1,41 @@
 #!/bin/bash
 
-# Retrieve Android version and clear terminal
-android_version=$(getprop ro.build.version.release)
-version_number=${android_version%%.*}
-clear
-
-# Setup Termux storage and update system
 termux-setup-storage
-apt update && apt upgrade -y
+apt update
+apt upgrade -y
 
-# Install required packages
 pkg install -y git python python-pip openjdk-17
+
 pip install gdown licensing mysql-connector-python requests
 
-# Clone the NgocRongTermux repository
 git clone https://github.com/JINN1368/NgocRongTermux
-
-# Remove unnecessary files
+clear
 echo "TIẾN HÀNH XOÁ TỆP THỪA"
 sleep 2
+
 cd NgocRongTermux
 rm -rf *.md
-
-# Download and set up the environment based on Android version
-if [ "$version_number" -gt 11 ]; then
-    echo "CÀI ĐẶT SERVER"
-    
-    # Clone and install Tamp
-    cd ~
-    git clone https://github.com/1Tech-X/Tamp
-    cd Tamp
-    
-    # Run installation scripts with error handling
-    bash install.sh || { echo "Install script failed"; exit 1; }
-    curl -LO https://raw.githubusercontent.com/1Tech-X/Tamp/main/update_tamp.sh
-    bash update_tamp.sh || { echo "Update script failed"; exit 1; }
-    curl -LO https://raw.githubusercontent.com/1Tech-X/Tamp/main/fix_phpmyadmin.sh
-    bash fix_phpmyadmin.sh || { echo "Fix phpMyAdmin script failed"; exit 1; }
-
-    clear
-    echo "ĐANG TẢI DỮ LIỆU"
-    
-    # Download and unzip data
-    gdown --quiet 1T7uB5HdciSWAvxF9vP_f1wqZXfVz76tc
-    unzip -q '*.zip'
-    rm -rf *.zip
-    
-    # Move scripts to /usr/bin
-    cd ~/NgocRongTermux
-    mv *.sh ~/../usr/bin/
-    chmod +x ~/../usr/bin/*.sh
-    
-    clear
-    jinn1368.sh
-
-else
-    echo "ĐANG TẢI DỮ LIỆU"
-    
-    # Download and unzip data
-    gdown --quiet 1T7uB5HdciSWAvxF9vP_f1wqZXfVz76tc
-    unzip -q '*.zip'
-    rm -rf *.zip
-    
-    # Move scripts to /usr/bin
-    cd ~/NgocRongTermux
-    mv *.sh ~/../usr/bin/
-    chmod +x ~/../usr/bin/*.sh
-    
-    echo "PHIÊN BẢN ANDROID CỦA BẠN KHÔNG HỖ TRỢ TAMPP, VUI LÒNG SETUP KSWEB."
-    jinn1368.sh
-fi
-
 clear
+
+cd
+echo "CÀI ĐẶT SERVER"
+git clone https://github.com/1Tech-X/Tamp
+cd Tamp
+bash install.sh
+curl -LO https://raw.githubusercontent.com/1Tech-X/Tamp/main/update_tamp.sh
+bash update_tamp.sh
+curl -LO https://raw.githubusercontent.com/1Tech-X/Tamp/main/fix_phpmyadmin.sh
+bash fix_phpmyadmin.sh
+clear
+
+echo "ĐANG TẢI DỮ LIỆU"
+gdown --quiet 1T7uB5HdciSWAvxF9vP_f1wqZXfVz76tc
+unzip *.zip
+rm -rf *.zip
+cd
+cd NgocRongTermux
+mv *.sh ~/../usr/bin/
+chmod +x ~/../usr/bin/*.sh
+cd
+clear
+jinn1368.sh
